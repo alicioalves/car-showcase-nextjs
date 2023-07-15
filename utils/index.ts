72109ab0b5e79d7461325bde3ac4a25d7ce03f47
Alicio Alves <alicio.alvesm@gmail.com>
2023-07-15 13:11:31 -0300

    chore: add car images API

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,17 @@
+import { CarProps } from '@/types'
 import axios from 'axios'
 
-const options = {
-  method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: { model: 'corolla' },
-  headers: {
-    'X-RapidAPI-Key': process.env.CARS_API_KEY,
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
-}
-
 export async function fetchCars() {
+  const options = {
+    method: 'GET',
+    url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    params: { model: 'corolla' },
+    headers: {
+      'X-RapidAPI-Key': process.env.CARS_API_KEY,
+      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+  }
+
   try {
     const response = await axios.request(options)
 
@@ -33,4 +34,21 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
   return rentalRatePerDay.toFixed(0)
+}
+
+// TODO: Replace this API
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/car-image-api')
+
+  const { make, year, model } = car
+
+  url.searchParams.append('customer', `${process.env.IMAGIN_STUDIO_API_KEY}`)
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
+
+  return `${url}`
 }
